4441fe24f6fc93fe15333e56b5a280d0e39893e0
Frank Denis <github@pureftpd.org>
2015-01-18 20:47:25 +0100

    Output buffers don't have to be defined

--- js-build/macros/output_buf.js
+++ js-build/macros/output_buf.js
@@ -1,7 +1,5 @@
 // ---------- output (buf)
 
-requireDefined(toDealloc, {var_name}, "{var_name}");
-
 var {var_name}_size = ({var_size}) | 0,
     {var_name} = new TargetBuf({var_name}_size),
     {var_name}_byte = {var_name}.address;
