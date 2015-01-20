169c3627592c0b62821592cb718e9dc6fa64b3c3
Frank Denis <github@pureftpd.org>
2015-01-20 23:07:57 +0100

    There is no such thing as encode_utf8()

--- js-build/macros/input_buf.js
+++ js-build/macros/input_buf.js
@@ -4,17 +4,13 @@ requireDefined(toDealloc, {var_name}, "{var_name}");
 
 var {var_name}_byte, {var_name}_size = ({var_size}) | 0;
 
-if ({var_name} instanceof Uint8Array) {
-	if ({var_name}.length !== {var_name}_size) {
-		throwTypeError(toDealloc, 'invalid {var_name} size');
-    }
-	{var_name}_byte = injectBytes({var_name});
-} else if (typeof {var_name} === 'string') {
-	if (encode_utf8({var_name}).length !== {var_name}_size) {
-		throwTypeError(toDealloc, 'invalid {var_name} size');
-    }
-    {var_name}_byte = injectBytes(string_to_Uint8Array({var_name}));
-} else {
-	throwTypeError(toDealloc, 'Unsupported input type for {var_name}');
+if (typeof {var_name} === 'string') {
+	{var_name} = string_to_Uint8Array({var_name});
+} else if (!({var_name} instanceof Uint8Array)) {
+	throwTypeError(toDealloc, 'unsupported input type for {var_name}');
 }
+if ({var_name}.length !== {var_name}_size) {
+	throwTypeError(toDealloc, 'invalid {var_name} size');
+}
+{var_name}_byte = injectBytes({var_name});
 toDealloc.push({var_name}_byte);
