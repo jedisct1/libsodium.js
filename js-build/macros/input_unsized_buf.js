859e8566a70d93bc6483ae7733596af3204e70a2
Frank Denis <github@pureftpd.org>
2015-01-20 23:13:25 +0100

    lowercase

--- js-build/macros/input_unsized_buf.js
+++ js-build/macros/input_unsized_buf.js
@@ -5,7 +5,7 @@ requireDefined(toDealloc, {var_name}, "{var_name}");
 if (typeof {var_name} === 'string') {
     {var_name} = string_to_Uint8Array({var_name});
 } else if (!({var_name} instanceof Uint8Array)) {
-	throwTypeError(toDealloc, 'Unsupported input type for {var_name}');
+	throwTypeError(toDealloc, 'unsupported input type for {var_name}');
 }
 var {var_name}_byte = injectBytes({var_name}),
     {var_name}_size = {var_name}.length;
