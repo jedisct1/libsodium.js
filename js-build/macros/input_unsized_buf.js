2c33074e561ec940a2ac0693d34a6451408937c8
Frank Denis <github@pureftpd.org>
2015-01-20 23:12:19 +0100

    Fix the length in input_unsized_buf when a string is not Latin1.

--- js-build/macros/input_unsized_buf.js
+++ js-build/macros/input_unsized_buf.js
@@ -2,13 +2,11 @@
 
 requireDefined(toDealloc, {var_name}, "{var_name}");
 
-var {var_name}_byte, {var_name}_size = {var_name}.length | 0;
-
-if ({var_name} instanceof Uint8Array) {
-	{var_name}_byte = injectBytes({var_name});
-} else if (typeof {var_name} === 'string') {
-	{var_name}_byte = injectBytes(string_to_Uint8Array({var_name}));
-} else {
+if (typeof {var_name} === 'string') {
+    {var_name} = string_to_Uint8Array({var_name});
+} else if (!({var_name} instanceof Uint8Array)) {
 	throwTypeError(toDealloc, 'Unsupported input type for {var_name}');
 }
+var {var_name}_byte = injectBytes({var_name}),
+    {var_name}_size = {var_name}.length;
 toDealloc.push({var_name}_byte);
