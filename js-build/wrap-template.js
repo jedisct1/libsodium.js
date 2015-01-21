475a713b623099b80c5036d6b005e2640a220f57
Frank Denis <github@pureftpd.org>
2015-01-21 10:06:07 +0100

    Avoid division

--- js-build/wrap-template.js
+++ js-build/wrap-template.js
@@ -41,8 +41,8 @@
 	function from_hex(s) {
 		if (!is_hex(s)) throw new TypeError('The provided string doesn\'t look like hex data');
 		var result = new Uint8Array(s.length / 2);
-		for (var i = 0; i < s.length / 2; i++) {
-			result[i] = parseInt(s.substr(2 * i, 2), 16);
+		for (var i = 0; i < s.length; i += 2) {
+			result[i] = parseInt(s.substr(i, 2), 16);
 		}
 		return result;
 	}
