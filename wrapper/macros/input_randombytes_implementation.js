916caaf772d42b433c71b379715b737eaffe7103
Frank Denis <github@pureftpd.org>
2015-01-25 15:34:59 +0100

    ' -> "

--- wrapper/macros/input_randombytes_implementation.js
+++ wrapper/macros/input_randombytes_implementation.js
@@ -4,6 +4,6 @@ var {var_name}_address = libsodium._malloc(6 * 4);
 for (var i = 0; i < 6; i++) {
 	libsodium.setValue({var_name}_address + i * 4,
 	    libsodium.Runtime.addFunction({var_name}
-	    [['implementation_name', 'random', 'stir', 'uniform', 'buf', 'close'][i]]),
-	    'i32');
+	    [["implementation_name", "random", "stir", "uniform", "buf", "close"][i]]),
+	    "i32");
 }
