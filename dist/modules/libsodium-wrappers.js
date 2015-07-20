80e0cbdb65ee94298e6b3a158b720be78981f4c6
Frank Denis <github@pureftpd.org>
2015-07-20 16:44:40 +0200

    Tabs, even though I hate them

--- dist/modules/libsodium-wrappers.js
+++ dist/modules/libsodium-wrappers.js
@@ -30,10 +30,10 @@
 		if (! bytes instanceof Uint8Array) {
 			throw new TypeError("Only Uint8Array instances can be incremented");
 		}
-        var c = 1 << 8;
+		var c = 1 << 8;
 		for (var i = 0 | 0, j = bytes.length; i < j; i++) {
-            c >>= 8;
-            c += bytes[i];
+			c >>= 8;
+			c += bytes[i];
 			bytes[i] = c & 0xff;
 		}
 	}
