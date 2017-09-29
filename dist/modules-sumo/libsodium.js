75af87e972896786aa8b24615e9690ebac2e3bb5
Frank Denis <github@pureftpd.org>
2017-09-29 20:38:23 +0200

    Regenified:   libsodium (modified content, untracked content)

--- dist/modules-sumo/libsodium.js
+++ dist/modules-sumo/libsodium.js
@@ -3,8 +3,12 @@
 function expose_libsodium(exports) {
     "use strict";
     var Module = exports;
-    var Module = Module || {};
+    if (typeof Module === 'undefined') {
+      var Module = {};
+    }
+    var _Module = Module;
     Module.ready = new Promise(function (resolve, reject) {
+      var Module = _Module;
       Module.onAbort = reject;
       Module.onRuntimeInitialized = resolve;
       // The Module object: Our interface to the outside world. We import
@@ -1631,6 +1635,8 @@ function integrateWasmJS() {
           throw "failed to load wasm binary file at '" + wasmBinaryFile + "'";
         }
         return response['arrayBuffer']();
+      }).catch(function () {
+        return getBinary();
       });
     }
     // Otherwise, getBinary should be able to get it synchronously
@@ -2701,78 +2707,76 @@ Module["intArrayFromString"] = intArrayFromString;
 Module["intArrayToString"] = intArrayToString;
 // All functions here should be maybeExported from jsifier.js
 
-if (typeof ENVIRONMENT_IS_SHELL === 'boolean' && ENVIRONMENT_IS_SHELL) {
-  // Copied from https://github.com/strophe/strophejs/blob/e06d027/src/polyfills.js#L149
+// Copied from https://github.com/strophe/strophejs/blob/e06d027/src/polyfills.js#L149
 
-  // This code was written by Tyler Akins and has been placed in the
-  // public domain.  It would be nice if you left this header intact.
-  // Base64 code from Tyler Akins -- http://rumkin.com
+// This code was written by Tyler Akins and has been placed in the
+// public domain.  It would be nice if you left this header intact.
+// Base64 code from Tyler Akins -- http://rumkin.com
 
-  var keyStr = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=';
+var keyStr = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=';
 
-  var btoa = function (input) {
-    /**
-     * Encodes a string in base64
-     * @param {String} input The string to encode in base64.
-     */
-    var output = '';
-    var chr1, chr2, chr3;
-    var enc1, enc2, enc3, enc4;
-    var i = 0;
-    do {
-      chr1 = input.charCodeAt(i++);
-      chr2 = input.charCodeAt(i++);
-      chr3 = input.charCodeAt(i++);
+var btoa = btoa || function (input) {
+  /**
+   * Encodes a string in base64
+   * @param {String} input The string to encode in base64.
+   */
+  var output = '';
+  var chr1, chr2, chr3;
+  var enc1, enc2, enc3, enc4;
+  var i = 0;
+  do {
+    chr1 = input.charCodeAt(i++);
+    chr2 = input.charCodeAt(i++);
+    chr3 = input.charCodeAt(i++);
 
-      enc1 = chr1 >> 2;
-      enc2 = ((chr1 & 3) << 4) | (chr2 >> 4);
-      enc3 = ((chr2 & 15) << 2) | (chr3 >> 6);
-      enc4 = chr3 & 63;
+    enc1 = chr1 >> 2;
+    enc2 = ((chr1 & 3) << 4) | (chr2 >> 4);
+    enc3 = ((chr2 & 15) << 2) | (chr3 >> 6);
+    enc4 = chr3 & 63;
 
-      if (isNaN(chr2)) {
-        enc2 = ((chr1 & 3) << 4);
-        enc3 = enc4 = 64;
-      } else if (isNaN(chr3)) {
-        enc4 = 64;
-      }
-      output = output + keyStr.charAt(enc1) + keyStr.charAt(enc2) + keyStr.charAt(enc3) + keyStr.charAt(enc4);
-    } while (i < input.length);
-    return output;
-  };
+    if (isNaN(chr2)) {
+      enc2 = ((chr1 & 3) << 4);
+      enc3 = enc4 = 64;
+    } else if (isNaN(chr3)) {
+      enc4 = 64;
+    }
+    output = output + keyStr.charAt(enc1) + keyStr.charAt(enc2) + keyStr.charAt(enc3) + keyStr.charAt(enc4);
+  } while (i < input.length);
+  return output;
+};
 
-  var atob = function (input) {
-    /**
-     * Decodes a base64 string.
-     * @param {String} input The string to decode.
-     */
-    var output = '';
-    var chr1, chr2, chr3;
-    var enc1, enc2, enc3, enc4;
-    var i = 0;
-    // remove all characters that are not A-Z, a-z, 0-9, +, /, or =
-    input = input.replace(/[^A-Za-z0-9\+\/\=]/g, '');
-    do {
-      enc1 = keyStr.indexOf(input.charAt(i++));
-      enc2 = keyStr.indexOf(input.charAt(i++));
-      enc3 = keyStr.indexOf(input.charAt(i++));
-      enc4 = keyStr.indexOf(input.charAt(i++));
+var atob = atob || function (input) {
+  /**
+   * Decodes a base64 string.
+   * @param {String} input The string to decode.
+   */
+  var output = '';
+  var chr1, chr2, chr3;
+  var enc1, enc2, enc3, enc4;
+  var i = 0;
+  // remove all characters that are not A-Z, a-z, 0-9, +, /, or =
+  input = input.replace(/[^A-Za-z0-9\+\/\=]/g, '');
+  do {
+    enc1 = keyStr.indexOf(input.charAt(i++));
+    enc2 = keyStr.indexOf(input.charAt(i++));
+    enc3 = keyStr.indexOf(input.charAt(i++));
+    enc4 = keyStr.indexOf(input.charAt(i++));
 
-      chr1 = (enc1 << 2) | (enc2 >> 4);
-      chr2 = ((enc2 & 15) << 4) | (enc3 >> 2);
-      chr3 = ((enc3 & 3) << 6) | enc4;
+    chr1 = (enc1 << 2) | (enc2 >> 4);
+    chr2 = ((enc2 & 15) << 4) | (enc3 >> 2);
+    chr3 = ((enc3 & 3) << 6) | enc4;
 
-      output = output + String.fromCharCode(chr1);
+    output = output + String.fromCharCode(chr1);
 
-      if (enc3 !== 64) {
-        output = output + String.fromCharCode(chr2);
-      }
-      if (enc4 !== 64) {
-        output = output + String.fromCharCode(chr3);
-      }
-    } while (i < input.length);
-    return output;
-  };
-}
+    if (enc3 !== 64) {
+      output = output + String.fromCharCode(chr2);
+    }
+    if (enc4 !== 64) {
+      output = output + String.fromCharCode(chr3);
+    }
+  } while (i < input.length);
+  return output;
+};
 
 // Converts a string of base64 into a byte array.
 // Throws error on invalid input.
@@ -2790,7 +2794,7 @@ function intArrayFromBase64(s) {
   try {
     var decoded = atob(s);
     var bytes = new Uint8Array(decoded.length);
-    for (i = 0 ; i < decoded.length ; ++i) {
+    for (var i = 0 ; i < decoded.length ; ++i) {
       bytes[i] = decoded.charCodeAt(i);
     }
     return bytes;
@@ -3095,6 +3099,7 @@ run();
 
 // {{MODULE_ADDITIONS}}
     }).catch(function () {
+      var Module = _Module;
       Module.onAbort = undefined;
       Module.onRuntimeInitialized = undefined;
       // The Module object: Our interface to the outside world. We import
@@ -44380,78 +44385,76 @@ Module["intArrayFromString"] = intArrayFromString;
 Module["intArrayToString"] = intArrayToString;
 // All functions here should be maybeExported from jsifier.js
 
-if (typeof ENVIRONMENT_IS_SHELL === 'boolean' && ENVIRONMENT_IS_SHELL) {
-  // Copied from https://github.com/strophe/strophejs/blob/e06d027/src/polyfills.js#L149
+// Copied from https://github.com/strophe/strophejs/blob/e06d027/src/polyfills.js#L149
 
-  // This code was written by Tyler Akins and has been placed in the
-  // public domain.  It would be nice if you left this header intact.
-  // Base64 code from Tyler Akins -- http://rumkin.com
+// This code was written by Tyler Akins and has been placed in the
+// public domain.  It would be nice if you left this header intact.
+// Base64 code from Tyler Akins -- http://rumkin.com
 
-  var keyStr = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=';
+var keyStr = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=';
 
-  var btoa = function (input) {
-    /**
-     * Encodes a string in base64
-     * @param {String} input The string to encode in base64.
-     */
-    var output = '';
-    var chr1, chr2, chr3;
-    var enc1, enc2, enc3, enc4;
-    var i = 0;
-    do {
-      chr1 = input.charCodeAt(i++);
-      chr2 = input.charCodeAt(i++);
-      chr3 = input.charCodeAt(i++);
+var btoa = btoa || function (input) {
+  /**
+   * Encodes a string in base64
+   * @param {String} input The string to encode in base64.
+   */
+  var output = '';
+  var chr1, chr2, chr3;
+  var enc1, enc2, enc3, enc4;
+  var i = 0;
+  do {
+    chr1 = input.charCodeAt(i++);
+    chr2 = input.charCodeAt(i++);
+    chr3 = input.charCodeAt(i++);
 
-      enc1 = chr1 >> 2;
-      enc2 = ((chr1 & 3) << 4) | (chr2 >> 4);
-      enc3 = ((chr2 & 15) << 2) | (chr3 >> 6);
-      enc4 = chr3 & 63;
+    enc1 = chr1 >> 2;
+    enc2 = ((chr1 & 3) << 4) | (chr2 >> 4);
+    enc3 = ((chr2 & 15) << 2) | (chr3 >> 6);
+    enc4 = chr3 & 63;
 
-      if (isNaN(chr2)) {
-        enc2 = ((chr1 & 3) << 4);
-        enc3 = enc4 = 64;
-      } else if (isNaN(chr3)) {
-        enc4 = 64;
-      }
-      output = output + keyStr.charAt(enc1) + keyStr.charAt(enc2) + keyStr.charAt(enc3) + keyStr.charAt(enc4);
-    } while (i < input.length);
-    return output;
-  };
+    if (isNaN(chr2)) {
+      enc2 = ((chr1 & 3) << 4);
+      enc3 = enc4 = 64;
+    } else if (isNaN(chr3)) {
+      enc4 = 64;
+    }
+    output = output + keyStr.charAt(enc1) + keyStr.charAt(enc2) + keyStr.charAt(enc3) + keyStr.charAt(enc4);
+  } while (i < input.length);
+  return output;
+};
 
-  var atob = function (input) {
-    /**
-     * Decodes a base64 string.
-     * @param {String} input The string to decode.
-     */
-    var output = '';
-    var chr1, chr2, chr3;
-    var enc1, enc2, enc3, enc4;
-    var i = 0;
-    // remove all characters that are not A-Z, a-z, 0-9, +, /, or =
-    input = input.replace(/[^A-Za-z0-9\+\/\=]/g, '');
-    do {
-      enc1 = keyStr.indexOf(input.charAt(i++));
-      enc2 = keyStr.indexOf(input.charAt(i++));
-      enc3 = keyStr.indexOf(input.charAt(i++));
-      enc4 = keyStr.indexOf(input.charAt(i++));
+var atob = atob || function (input) {
+  /**
+   * Decodes a base64 string.
+   * @param {String} input The string to decode.
+   */
+  var output = '';
+  var chr1, chr2, chr3;
+  var enc1, enc2, enc3, enc4;
+  var i = 0;
+  // remove all characters that are not A-Z, a-z, 0-9, +, /, or =
+  input = input.replace(/[^A-Za-z0-9\+\/\=]/g, '');
+  do {
+    enc1 = keyStr.indexOf(input.charAt(i++));
+    enc2 = keyStr.indexOf(input.charAt(i++));
+    enc3 = keyStr.indexOf(input.charAt(i++));
+    enc4 = keyStr.indexOf(input.charAt(i++));
 
-      chr1 = (enc1 << 2) | (enc2 >> 4);
-      chr2 = ((enc2 & 15) << 4) | (enc3 >> 2);
-      chr3 = ((enc3 & 3) << 6) | enc4;
+    chr1 = (enc1 << 2) | (enc2 >> 4);
+    chr2 = ((enc2 & 15) << 4) | (enc3 >> 2);
+    chr3 = ((enc3 & 3) << 6) | enc4;
 
-      output = output + String.fromCharCode(chr1);
+    output = output + String.fromCharCode(chr1);
 
-      if (enc3 !== 64) {
-        output = output + String.fromCharCode(chr2);
-      }
-      if (enc4 !== 64) {
-        output = output + String.fromCharCode(chr3);
-      }
-    } while (i < input.length);
-    return output;
-  };
-}
+    if (enc3 !== 64) {
+      output = output + String.fromCharCode(chr2);
+    }
+    if (enc4 !== 64) {
+      output = output + String.fromCharCode(chr3);
+    }
+  } while (i < input.length);
+  return output;
+};
 
 // Converts a string of base64 into a byte array.
 // Throws error on invalid input.
@@ -44469,7 +44472,7 @@ function intArrayFromBase64(s) {
   try {
     var decoded = atob(s);
     var bytes = new Uint8Array(decoded.length);
-    for (i = 0 ; i < decoded.length ; ++i) {
+    for (var i = 0 ; i < decoded.length ; ++i) {
       bytes[i] = decoded.charCodeAt(i);
     }
     return bytes;
