9fef3fe5bd16581892b59df8cd25b9c424df8c8e
Frank Denis <github@pureftpd.org>
2018-05-12 17:49:17 +0200

    Check that process.removeAllListeners is a function
    Reindent by the way.
    
    Maybe
    Fixes #152

--- wrapper/libsodium-post.js
+++ wrapper/libsodium-post.js
@@ -1,13 +1,16 @@
-    if (typeof(process) === 'object') { process.removeAllListeners("uncaughtException"); process.removeAllListeners("unhandledRejection"); }
+    if (typeof(process) === 'object' && typeof(process.removeAllListeners) === 'function') {
+      process.removeAllListeners('uncaughtException');
+      process.removeAllListeners('unhandledRejection');
+    }
     return Module;
 }
 
 if (typeof define === 'function' && define.amd) {
-  define(['exports'], expose_libsodium);
+    define(['exports'], expose_libsodium);
 } else if (typeof exports === 'object' && typeof exports.nodeName !== 'string') {
-  expose_libsodium(exports);
+    expose_libsodium(exports);
 } else {
-  root.libsodium = expose_libsodium(root.libsodium_mod || (root.commonJsStrict = {}));
+    root.libsodium = expose_libsodium(root.libsodium_mod || (root.commonJsStrict = {}));
 }
 
 })(this);
