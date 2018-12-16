522069040a4078b79f8815c440e61ec94e7d9edc
NullVoxPopuli <LPSego3+dev@gmail.com>
2018-12-16 17:35:29 -0500

    will this work?

--- emberclear/build.sh
+++ emberclear/build.sh
@@ -7,4 +7,11 @@ cp emberclear/emscripten.sh libsodium/dist-build/emscripten.sh
 
 make
 
-echo "TODO: push repo stuff"
+git config --global user.email "travis@travis-ci.org"
+git config --global user.name "Travis CI"
+git checkout -b emberclear
+git remote rm origin
+git remote add origin https://NullVoxPopuli:${GH_TOKEN}@github.com/NullVoxPopuli/libsodium.js.git
+git add .
+git commit -am"build smaller libsodium"
+git push origin emberclear -f
