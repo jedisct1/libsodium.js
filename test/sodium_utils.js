7948d7cae227bbd280870e4f73833f0c27b75529
Paragon Initiative Enterprises <security@paragonie.com>
2019-10-09 15:13:50 -0400

    Add unit test

--- test/sodium_utils.js
+++ test/sodium_utils.js
@@ -16,5 +16,13 @@ describe('sodium_utils', () => {
         sodium.add(big, one);
         expect(big.toString('hex')).to.be.equals('00010000');
     });
+
+    it('crypto_kdf_derive_from_key', async() => {
+        if (!sodium) sodium = await test_helper.init();
+        let key = Buffer.from('808182838485868788898a8b8c8d8e8f909192939495969798999a9b9c9d9e9f', 'hex');
+        let subkey = libsodium.crypto_kdf_derive_from_key(32, 1, 'NaClTest', key);
+        expect(Buffer.from(wrong).toString('hex'))
+            .to.be.equals('bce6fcf118cac2691bb23975a63dfac02282c1cd5de6ab9febcbb0ec4348181b');
+    });
 });
 
