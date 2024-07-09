cf1ee09b5b82aeceb4d803894c8f7817dd75e6d4
Frank Denis <github@pureftpd.org>
2024-07-09 15:51:13 +0200

    Reimplement more tests

--- test/sodium.test.ts
+++ test/sodium.test.ts
@@ -205,3 +205,38 @@ test("crypto_sign", () => {
     const verified3 = sodium.crypto_sign_verify_detached(signature, message2, publicKey);
     expect(verified3).toBe(false);
 });
+
+test("crypto_sign_ed25519_to_curve25519", () => {
+    const secretkey = sodium.from_hex('411a2c2227d2a799ebae0ed94417d8e8ed1ca9b0a9d5f4cd743cc52d961e94e2' +
+        'da49154c9e700b754199df7974e9fa4ee4b6ebbc71f89d8d8938335ea4a1409d');
+    const publickey = sodium.from_hex('da49154c9e700b754199df7974e9fa4ee4b6ebbc71f89d8d8938335ea4a1409d');
+    const x25519SecretKey = sodium.crypto_sign_ed25519_sk_to_curve25519(secretkey);
+    const x25519PublicKey = sodium.crypto_sign_ed25519_pk_to_curve25519(publickey);
+    expect(sodium.to_hex(x25519SecretKey)).toBe('60c783b8d1674b7081b72a105b55872502825d4ec638028152e085b54705ad7e');
+    expect(sodium.to_hex(x25519PublicKey)).toBe('5a791d07cfb39060c8e9b641b6a915a3126cd14ddc243a9928c490c8e1f59e7c');
+});
+
+test("randombyte_buf", () => {
+    for (let i = 0; i < 100; i++) {
+        const a = sodium.randombytes_buf(100);
+        const b = sodium.randombytes_buf(100);
+        expect(a).not.toEqual(b);
+    }
+});
+
+test("randombyte_uniform", () => {
+    for (let i = 0; i < 10; i++) {
+        const a = sodium.randombytes_uniform(0x3fffffff);
+        const b = sodium.randombytes_uniform(0x3fffffff);
+        expect(a).not.toEqual(b);
+    }
+});
+
+test("sodium_compare", () => {
+    const a = sodium.from_string('Science, math, technology, engineering, and compassion for others');
+    const b = sodium.from_string('Science, math, technology, engineering, and compassion for others');
+    const c = sodium.from_string('Science, math, technology, engineering, and compassion for OTHERS');
+    expect(sodium.compare(a, b)).toBe(0);
+    expect(sodium.compare(a, c)).toBe(1);
+    expect(sodium.compare(c, b)).toBe(-1);
+});
